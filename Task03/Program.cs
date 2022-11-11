d802d2015ceb5ff5f55425c0f736e9a8aa1167df
OlgaShestakova <olgashestakova@gmail.com>
2022-11-11 12:52:10 +0300

    Add Task03, Task05, Task07

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели:");
+string day = Console.ReadLine();
+if (day == "1") Console.WriteLine($"День недели под номером {day} - понедельник");
+else if (day == "2") Console.WriteLine($"День недели под номером {day} - вторник");
+else if (day == "3") Console.WriteLine($"День недели под номером {day} - среда");
+else if (day == "4") Console.WriteLine($"День недели под номером {day} - четверг");
+else if (day == "5") Console.WriteLine($"День недели под номером {day} - пятница");
+else if (day == "6") Console.WriteLine($"День недели под номером {day} - суббота");
+else if (day == "7") Console.WriteLine($"День недели под номером {day} - воскресенье");
+else Console.WriteLine("Нет дня недели с данным номером");
---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+Console.WriteLine($"Ряд чисел от {count} до {number}:");
+while(count <= number)
+{
+    Console.Write(count + " ");
+    count += 1;
+}
