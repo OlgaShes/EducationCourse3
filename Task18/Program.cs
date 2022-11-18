639e699dafef0d10fe162732c0972cf78623cc8c
OlgaShestakova <olgashestakova@gmail.com>
2022-11-18 12:15:42 +0300

    Add Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// По заданному номеру четверти показывем диапазон возможных точек четверти
+// 2 | 1
+// -----
+// 3 | 4
+
+Console.Clear();
+
+Console.Write("Ведите номер четверти: ");
+string quoter = Console.ReadLine();
+
+string Range(string quot)
+{
+    if (quot == "1") return "x > 0, y > 0";
+    if (quot == "2") return "x < 0, y < 0";
+    if (quot == "3") return "x < 0, y < 0";
+    if (quot == "4") return "x > 0, y < 0";
+    return "Неверно задана четверть";
+}
+
+Console.WriteLine(Range(quoter));
