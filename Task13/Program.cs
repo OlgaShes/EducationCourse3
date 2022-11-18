5df0b277676b35e12b94a20ea569b419d99606f2
OlgaShestakova <olgashestakova@gmail.com>
2022-11-18 10:08:54 +0300

    Eddit Task 13, turning on version 2

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -9,32 +9,32 @@ Console.Clear();
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int LastDigit(int numb)
-{
-    return Math.Abs(number) % 10;
-}
-
-if (Math.Abs(number) < 100) Console.WriteLine("Третьей цифры нет");
-else
-{
-    while (Math.Abs(number) > 999)
-    {
-        number = number / 10;
-    }
-    Console.WriteLine("Третья цифра числа --> " + LastDigit(number));
-}
-
-// int DigitInPos(int numb, int pos)
+// int LastDigit(int numb)
 // {
-//     if (Math.Abs(numb) < Math.Pow(10, pos - 1)) return -1;
-//     while (Math.Abs(numb) >=  Math.Pow(10, pos))
-//         {
-//             numb = numb / 10;
-//         }
-//     return Math.Abs(numb) % 10;
+//     return Math.Abs(number) % 10;
 // }
 
-// int position = 3;
-// int result = DigitInPos(number, position);
-// if (result == -1) Console.WriteLine($"В числе нет цифры под номером {position}");
-// else Console.WriteLine($"В данном числе {position} по счету цифра --> {result}");
+// if (Math.Abs(number) < 100) Console.WriteLine("Третьей цифры нет");
+// else
+// {
+//     while (Math.Abs(number) > 999)
+//     {
+//         number = number / 10;
+//     }
+//     Console.WriteLine("Третья цифра числа --> " + LastDigit(number));
+// }
+
+int DigitInPos(int numb, int pos)
+{
+    if (Math.Abs(numb) < Math.Pow(10, pos - 1)) return -1;
+    while (Math.Abs(numb) >=  Math.Pow(10, pos))
+        {
+            numb = numb / 10;
+        }
+    return Math.Abs(numb) % 10;
+}
+
+int position = 3;
+int result = DigitInPos(number, position);
+if (result == -1) Console.WriteLine($"В числе нет цифры под номером {position}");
+else Console.WriteLine($"В данном числе {position} по счету цифра --> {result}");
