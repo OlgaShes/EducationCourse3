43cffc937ea5d2e847743b7d24ccfc7a3d645754
OlgaShestakova <olgashestakova@gmail.com>
2022-11-18 12:15:56 +0300

    Add Task 20

---
+++ Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимаем на вход координаты двух точек и находит расстояние между ними
+// A(3, 6) B(2, 1)  5.09
+
+Console.Clear();
+
+Console.WriteLine("Ведите координаты точки А");
+Console.Write("X: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите координаты точки B");
+Console.Write("X: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double Length(int x1, int y1, int x2, int y2)
+{
+    double distance = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return Math.Round(distance, 2, MidpointRounding.ToZero);
+}
+
+Console.WriteLine(Length(xa, ya, xb, yb));
