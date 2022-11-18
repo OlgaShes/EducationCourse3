46b3455735691e1633b84c7b67170876378b892a
OlgaShestakova <olgashestakova@gmail.com>
2022-11-18 12:16:15 +0300

    Add Task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимаем на вход N и выдаем таблицу квадратов от 1 до N
+// n = 5
+// 1 1
+// 2 4
+// 3 9
+// 4 16
+// 5 25
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Squares(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(i + " -> " + i * i);
+    }
+}
+
+if (number < 1) Console.WriteLine("Ведено неверное число");
+else Squares(number);
