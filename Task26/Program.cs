6a31053ec9e7584c35c59e96c6341ab61d5746e5
OlgaShestakova <olgashestakova@gmail.com>
2022-11-22 12:26:35 +0300

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принимаем на вход число и выдаем количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89754 -> 5
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CountDigits(int num)
+{
+    int count = 1;
+    num = Math.Abs(num);
+    while (num >= 10)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+
+int countDigits = CountDigits(number);
+Console.WriteLine($"Количество цифр в числе {number} --> {countDigits}");
