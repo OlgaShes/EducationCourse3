c86ae09a8ea36c3e6ee6cfcf0aa7da8407e9e45c
OlgaShestakova <olgashestakova@gmail.com>
2022-11-22 12:26:49 +0300

    Add Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимаем число N, выдаем произведение чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multiplication(int num)
+{
+    int result = 1;
+    for (int i = 1; i < num + 1; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+if (number > 0)
+{
+int mult = Multiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {mult}");
+}
+else Console.WriteLine("Введено некорректное число");
