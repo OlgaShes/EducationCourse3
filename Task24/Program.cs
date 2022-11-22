914e6328d0bc3e44ecc3668969a1e903d4623539
OlgaShestakova <olgashestakova@gmail.com>
2022-11-22 12:26:19 +0300

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимаем число А и выдаем сумму чисел от 1 до А
+
+Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+if (number > 0)
+{
+    int sum = Sum(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+}
+else Console.WriteLine("Введено некорректное число");
