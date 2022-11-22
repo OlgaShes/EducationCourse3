6d02869e9796c39df6278bb86db25d102ddb4f7a
OlgaShestakova <olgashestakova@gmail.com>
2022-11-22 09:52:29 +0300

    Editing Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,28 +10,28 @@ Console.Clear();
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-bool Palindrome(int numb)
-{
-    int firstDigit = numb / 10000;
-    int secondDigit = numb / 1000 % 10;
-    int fourthDigit = numb / 10 % 10;
-    int fifthDigit = numb % 10;
-    return firstDigit == fifthDigit && secondDigit == fourthDigit ? true : false;
-}
-
-if (Math.Abs(number) < 10000 || Math.Abs(number) > 99999) Console.WriteLine("Введено не пятизначное число");
-else Console.WriteLine(Palindrome(number) ? "Число является палиндромом" : "Число не является палиндромом");
-
-// bool PalindromeGeneral(int numb)
+// bool Palindrome(int numb)
 // {
-//     int count = numb;
-//     int newNumb = 0;
-//     while (count != 0)
-//     {
-//         newNumb = newNumb * 10 + count % 10;
-//         count = count / 10;
-//     }
-//     return numb == newNumb ? true : false;
+//     int firstDigit = numb / 10000;
+//     int secondDigit = numb / 1000 % 10;
+//     int fourthDigit = numb / 10 % 10;
+//     int fifthDigit = numb % 10;
+//     return firstDigit == fifthDigit && secondDigit == fourthDigit;
 // }
 
-// Console.WriteLine(PalindromeGeneral(number) ? "Число является палиндромом" : "Число не является палиндромом");
+// if (Math.Abs(number) < 10000 || Math.Abs(number) > 99999) Console.WriteLine("Введено не пятизначное число");
+// else Console.WriteLine(Palindrome(number) ? "Число является палиндромом" : "Число не является палиндромом");
+
+bool PalindromeGeneral(int numb)
+{
+    int count = numb;
+    int newNumb = 0;
+    while (count != 0)
+    {
+        newNumb = newNumb * 10 + count % 10;
+        count = count / 10;
+    }
+    return numb == newNumb;
+}
+
+Console.WriteLine(PalindromeGeneral(number) ? "Число является палиндромом" : "Число не является палиндромом");
