b1512f5318467be4eefb916192530316c49a299a
OlgaShestakova <olgashestakova@gmail.com>
2022-11-25 12:51:23 +0300

    Add Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте масиив из 12 случайных элементов из промежутка [-9, 9] 
+// найти сумму отрицательных и положительных элементов
+
+// Напрмер:
+// В массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна -20
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i< arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumNegativeElem(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int GetSumPositiveElem(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(15, -40, 99);
+Console.Write("В массиве ");
+PrintArray(array);
+int sumNegative = GetSumNegativeElem(array);
+int sumPositive = GetSumPositiveElem(array);
+Console.WriteLine($"сумма положительных чисел равна {sumPositive}, сумма отрицательных равна {sumNegative}");
+
+
