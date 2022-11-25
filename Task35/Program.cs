d625b0ee7d7447b0a9c14bbd1e674e2db309a24f
OlgaShestakova <olgashestakova@gmail.com>
2022-11-25 12:52:09 +0300

    Add Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив из 123 случайных чисел
+// Найдите количество элементов в отрезке [10, 99]
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i< arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayRndInt(15, 0, 500);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+int FindAmountNumbers(int[] arr, int minNumber, int maxNumber)
+{
+    int amount = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= minNumber && arr[i] <= maxNumber) amount++;
+    }
+    return amount;
+}
+
+int result = FindAmountNumbers(array, 10, 99);
+Console.WriteLine(result > 0 ? 
+                  $"В массиве {result} чисел из заданного диапазона": 
+                    "В массивет нет чисел из указанного дапазона");
+
