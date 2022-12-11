003d324c0f7c1d001d23320e00ee6840ad4fb908
OlgaShestakova <olgashestakova@gmail.com>
2022-12-11 14:27:31 +0300

    Add Homework for Lesson 9

---
+++ Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: 
+// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+if (number1 < 1 && number2 < 1) sum = 0;
+else if (number1 < 1 && number2 > 0) sum = SumNaturalNumbers(0, number2);
+else if (number1 > 0 && number2 < 1) sum = SumNaturalNumbers(0, number1);
+else if (number1 <= number2) sum = SumNaturalNumbers(number1, number2);
+else sum = SumNaturalNumbers(number2, number1);
+
+if (sum == 0) Console.WriteLine("Натуральных чисел в промежутке нет");
+else Console.WriteLine($"Сумма натуральных числе в промежутке от {number1} до {number2} равна {sum}");
+
+int SumNaturalNumbers(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    else
+    {
+        return num1 + SumNaturalNumbers(num1 + 1, num2);
+    }
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Clear();
+
+Console.Write("Введите первое неотрицательное число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе неотрицательное число N: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 0 || number2 < 0) System.Console.WriteLine("Оба числа должны быть неотрицательными");
+else
+{
+    int ackerman = Ackerman(number1, number2);
+    Console.WriteLine("Результат вычисления функции Аккермана:");
+    Console.WriteLine($"A({number1}, {number2}) = {ackerman}");
+}
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Ackerman(m - 1, 1);
+    else return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
